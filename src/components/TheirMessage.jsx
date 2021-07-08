7094a6a12e7fcfee0fddd0dc1d109f4210655a26
bettercallsully <solomonelkelani@gmail.com>
2021-07-07 22:42:00 -0600

    Add customization to 3rd party user chat messages

--- src/components/TheirMessage.jsx
+++ src/components/TheirMessage.jsx
@@ -1,8 +1,16 @@
-const TheirMessage = () => {
+const TheirMessage = ({ lastMessage, message }) => {
+    const isFirstMessageByUser = !lastMessage || lastMessage.sender.username === message.sender.username;
     return (
-        <div>
-            TheirMessage
-        </div>
+    <div className="message-row">
+        {isFirstMessageByUser && (
+            <div
+               className="message-avatar"
+               style={{backgroundImage: `url(${message?.sender?.avatar})`}
+               
+               }
+               
+            )}
+     </div>
     );
 }
 
